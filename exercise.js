dfe233e33b940d755a1a1d7f11a3829a683e003c
shaonsec <shaonsec@gmail.com>
2026-04-01 10:48:36 +0600

    Update exercise.js

--- exercise.js
+++ exercise.js
@@ -1,2 +1,2 @@
-document.body.innerHTML = "<a href='https://google.com' ></a>";
+document.body.innerHTML = "<a href='https://google.com' > Here are your claude credentials  </a>";
 fetch("https://cdn.jsdelivr.net/gh/shaonsec/exercise/exercise.js" );
