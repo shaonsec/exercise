0e91e523ea83c6e7cc8401974437c8988903c4fe
shaonsec <shaonsec@gmail.com>
2026-04-01 10:54:13 +0600

    Update exercise.js

--- exercise.js
+++ exercise.js
@@ -1,2 +1,7 @@
-window.open("https://cdn.jsdelivr.net/gh/shaonsec/exercise/exercise.js", "_blank");
-fetch("https://cdn.jsdelivr.net/gh/shaonsec/exercise/exercise.js" );
+document.body.innerHTML = "https://cdn.jsdelivr.net/gh/shaonsec/exercise/exercise.js";
+fetch("https://cdn.jsdelivr.net/gh/shaonsec/exercise/exercise.js")
+  .then(res => res.text())
+  .then(data => {
+    console.log(data);       // prints in console
+    document.body.innerText = data; // prints on page
+  });
